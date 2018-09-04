539db1c13dfd448d6aab573cbb3b188bf02d1fd8
ch4rliem <31392462+ch4rliem@users.noreply.github.com>
2018-09-04 10:10:29 -0500

    Add files via upload

---
+++ learn-to-code.js
@@ -0,0 +1,101 @@
+// var name = 'Jack';
+// var age= 23;
+// var message = 'Hi, my name is ' + name + ' and I am ' + age + ' years old!';
+
+// console.log(message);
+
+// var firstName = 'John';
+// var lastName = 'JacobSmith'
+// var dateOfBirth = '10-09-82';
+// var age= 23;
+// var profileImgUrl = 'http://coolpicks.com/johnjacob.jpg';
+
+// var loginWelcolmeMessage = 'Welcome, ' + firstName + '. Happy ' + age + 'rd birthday!';
+
+// console.log(loginWelcolmeMessage);
+
+// var sum = 10 + 15;
+// var sub = 15 -10;
+// var mul = 10 * 3;
+// var div = 10 / 3;
+// var mod = 10 % 3;
+
+// var msg = "10 / 3 = 3 with a remainder of " + mod; 
+// console.log(message);
+
+// var myAccountBalance = 300;
+// var nikeSBShoes = 799.23;
+// var coupon= 500;
+// if (nikeSBShoes <= myAccountBalance) {
+//     //myAccountBalance = myAccountBalance - nikeSBShoes;
+//     myAccountBalance -= nikeSBShoes;
+//     console.log("we just bought some nice shoes!");
+//     console.log("Account Balance: " + myAccountBalance);
+// }
+// else if (nikeSBShoes - coupon <= myAccountBalance) {
+//     console.log("we just bought some nice shoes with a coupon!");
+//     myAccountBalance -= nikeSBShoes - coupon;
+//     console.log("Account Balance: " + myAccountBalance);
+// } else {
+//     console.log("You too broke for shoes broww!");
+
+// }
+
+// if (1 === 1 && 2 === 2) {
+//     console.log("These are both true");
+// }
+
+// if (true && true) {
+//     console.log("These are the same");
+// }
+
+// if (1 === 3 || "joe" === "joe") {
+//     console.log("One of these are true!");
+// }
+
+// var cat1 = 5;
+// var cat2 = 10;
+// var cat3 = 1;
+// var cat3DisabledHandicap = true;
+
+// if ((cat1 > cat2 && cat1 > cat3) && !cat3DisabledHandicap){
+//     console.log("Cat 1 is the cutest!");
+// }
+// else if ((cat2 > cat1 && cat2 > cat3) && !cat3DisabledHandicap){
+//     console.log("Cat 2 is the cutest!");
+
+// } else if ((cat3 > cat1 && cat3) > cat2 || cat3DisabledHandicap){
+//     console.log("Cat 3 is the cutest!");
+// }
+
+
+// var students = ["Timmy", "Janessa","Aron"];
+
+// var naughtyList = [];
+
+
+// naughtyList.push(students[0]);
+
+// var index = naughtyList.indexOf("Timmy");
+// //var index2 = students.indexOf("Aron");
+
+// console.log(naughtyList);
+// if (index > -1) {
+//      naughtyList.splice(index, 1);
+// }
+
+// console.log(naughtyList);
+
+//console.log(index2);
+
+// var total = 10;
+// for (var x = 0; x < total; x++) {
+//     console.log(x);
+// }
+
+var students = ["John", "Jacbo", "Jingle","Heimer", "Smith"];
+
+for (var index = 0; index < students.length; index++){
+    console.log(students[index]);
+}
+
